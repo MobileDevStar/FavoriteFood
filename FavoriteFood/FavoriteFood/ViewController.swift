6b9903018605e2bf83e7efc7ae3f76688d27a207
ITDevStar <itdevstar@hotmail.com>
2019-05-21 16:18:46 +0300

    src start

---
+++ FavoriteFood/FavoriteFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FavoriteFood
+//
+//  Created by Simba on 5/20/19.
+//  Copyright © 2019 Simba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
